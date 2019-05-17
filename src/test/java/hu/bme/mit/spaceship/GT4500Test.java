7d0aa972dee34652398a63ea87c009e364d2a7bf
tmst96 <tamassimon96@gmail.com>
2019-05-17 00:32:14 -0700

    mock test 7

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,21 +8,17 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
-  @Mock
   private GT4500 ship;
-  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule(); 
+
   @BeforeEach
   public void init(){
-
     this.ship = new GT4500(mock(TorpedoStore.class), mock(TorpedoStore.class));
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-    when(ship.getPrimaryTorpedoStore().getTorpedoCount().thenReturn());
-    ship.getPrimaryTorpedoStore().getTorpedoCount();
-    verify(ship.getPrimaryTorpedoStore(), times(1)).getTorpedoCount();
+
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
@@ -33,9 +29,7 @@ public class GT4500Test {
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-    when(ship.getPrimaryTorpedoStore().getTorpedoCount().thenReturn());
-    ship.getPrimaryTorpedoStore().getTorpedoCount();
-    verify(ship.getPrimaryTorpedoStore(), times(1)).getTorpedoCount();
+
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
